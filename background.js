5ae759ee919a3487b514c341a935a63946aa61b3
Bob LIU <lesignbuture@gmail.com>
2014-03-17 22:31:47 +0800

    Update background.js

--- background.js
+++ background.js
@@ -16,7 +16,7 @@
     urls: [
       "http://cdn.sstatic.net/Js/stub.en.js?*",
       "http://cdn.sstatic.net/stackoverflow/all.css?*",
-	  "http://cdn.sstatic.net/ux/all.css?*"
+      "http://cdn.sstatic.net/ux/all.css?*"
     ]
   },
   //extraInfoSpec
